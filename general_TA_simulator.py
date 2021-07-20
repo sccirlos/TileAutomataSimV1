d377b85cd6842bae83385415f37f8a7b8af430b2
egrizzell <elisegrizzell@gmail.com>
2021-07-20 12:42:40 -0500

    general simulator

---
+++ general_TA_simulator.py
@@ -0,0 +1,16 @@
+#General Seeded TA Simulator 
+import turtle
+
+#Takes in:
+    #Seed State 
+    #Set of States
+    #Set of Transition Rules
+    #Set of Affinities
+
+#Outputs:
+    #GUI showing step by step growth starting with seed state
+    #Step button
+    #Keep growing until their are no more rules that apply
+
+
+         
