f3b90b09ec1418e0921e01f2a22daafcf1158048
Tim Gomez <tagomez62@gmail.com>
2021-08-27 15:03:25 -0500

    Added Functions to AssemblyEnginer

--- assemblyEngine.py
+++ assemblyEngine.py
@@ -1,45 +1,58 @@
-from random import randrange
+import random 
 
-from UniversalClasses import System
-import TAMainWindow
-import LoadFile
-import SaveFile
-import Assembler_Proto
+import UniversalClasses
+
 
 import sys
 
-# Global Variables
-currentSystem = None
-
 class Engine:
-    def __init__(self):
+    def __init__(self, currentSystem):
         self.system = currentSystem
         self.assemblyList = []
         self.currentIndex = 0
         self.lastIndex = 0
 
         # Get seed 
+        seedState = random.choice(self.system.returnSeedStates())
+        seed = UniversalClasses.Tile(seedState, 0, 0) 
+        seedAssembly = UniversalClasses.Assembly()
+        seedAssembly.set_tiles([seed])
+        self.assemblyList.append(seedAssembly)
 
     def step(self):
         if(self.currentIndex < self.lastIndex): 
-            self.currentSystem = self.currentSystem + 1
+            self.currentIndex = self.currentIndex + 1
+            return 0
         else:
-            if(self.build() != -1):
-                self.currentSystem = self.currentSystem + 1
+            return self.build()
+                
 
     def back(self):
-        if(self.currentIndex > 0): self.currentSystem = self.currentSystem - 1
+        if(self.currentIndex > 0): self.currentIndex = self.currentIndex - 1
 
-    def getCurrAssembly(self):
+    def getCurrentAssembly(self):
         return self.assemblyList[self.currentIndex]
 
+    def getCurrentIndex(self):
+        return self.currentIndex
+
     def build(self):
+        # Get current Assembly
+        cAssembly = self.getCurrentAssembly()
+        moveList = cAssembly.getMoves(self.system)
+
         # Check if assembly is terminal
+        if(moveList.length() == 0): return -1
+
 
         # Update lastIndex
+        self.lastIndex = self.lastIndex + 1
+        self.currentIndex = self.currentIndex + 1
 
         # Get next assembly and add to list
-
-        return
+        move = random.choice(moveList)
+        newAssembly = cAssembly.performMove(move)
+        self.assemblyList.append(newAssembly)
+        return 0
 
     
