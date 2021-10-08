ff343aa8c43cd114efaf2580ec210e5f60e6cd2a
Andrew Rodriguez <aroday23@gmail.com>
2021-10-08 13:05:18 -0500

    fix crash after sim restart TimeTaken list needs to be saved in the history as well. This commits adds that which fixes the crashing.

--- Historian.py
+++ Historian.py
@@ -7,9 +7,10 @@ from PyQt5.QtWidgets import QFileDialog
 class Historian:
 
     class Assemblies:
-        def __init__(self, movelist, assembly):
+        def __init__(self, movelist, assembly, timetaken):
             self.movelist = movelist
             self.assembly = assembly
+            self.timetaken = timetaken
 
     def __init__(self):
         pass
@@ -25,17 +26,12 @@ class Historian:
 
         if filename[0] != '':
             fp = open(filename[0], 'w')
-            assemblies = self.Assemblies(self.engine.moveList, self.engine.currentAssembly)
+            assemblies = self.Assemblies(self.engine.moveList, self.engine.currentAssembly, self.engine.TimeTaken)
             # Dumping into one line saves a ton of space, but becomes completly unreadable, worse than it already is
             # json.dump(assemblies, fp, sort_keys=False, default=self.encoder)
             # Dumping with some indentation makes it nice-ish, but takes a large amount of space for \n and \t and ' '
             json.dump(assemblies, fp, sort_keys=False, default=self.encoder, indent=2)
 
-    def dumps(self):
-        print("Dumping Assemblies")
-        assemblies = self.Assemblies(self.engine.moveList, self.engine.currentAssembly)
-        print(json.dumps(assemblies, sort_keys=False, default=self.encoder, indent=2))
-    
     def load(self):
         filename = QFileDialog.getOpenFileName(self.ui, "Select JSON History", "", "JSON Files (*.json)")
 
@@ -117,6 +113,16 @@ class Historian:
             self.engine.currentAssembly = currentAssembly
             self.engine.validMoves = currentAssembly.getMoves(self.engine.system)
 
+            # create timetaken list
+            timetaken = assemblies["timetaken"]
+            TimeTaken = []
+            for tt in timetaken:
+                val = int(tt)
+                TimeTaken.append(val)
+            
+            # update TimeTake in engine
+            self.engine.TimeTaken = timetaken
+
             # draw to screen
             self.ui.draw_assembly(self.engine.getCurrentAssembly())
             self.ui.Update_available_moves()
@@ -136,38 +142,6 @@ class Historian:
 
         return tile
 
-    def loads(self):
-        print("Loading Assembiles")
-        moveHistoryEncoded = json.dumps(self.engine.moveList, sort_keys=False, default=self.encoder, indent=3)
-        moveHistoryDecoded = json.loads(moveHistoryEncoded)
-
-        print("History decoded into...")
-        print(*moveHistoryDecoded)
-
-        # Convert moveHistoryDecoded from dictionary to actual object it represents (state dictionary into state object)
-
-        eq = moveHistoryEncoded == json.dumps(moveHistoryDecoded, sort_keys=False, default=self.encoder, indent=3)
-
-        if eq:
-            print("Strings are the same! History Decode PASS")
-        else:
-            print("String are NOT the same! History Decode FAIL")
-        
-        assemblyEncoded = json.dumps(self.engine.currentAssembly, sort_keys=False, default=self.encoder, indent=3)
-        assemblyDecoded = json.loads(assemblyEncoded)
-
-        print("Assembly decoded into...")
-        print(*assemblyDecoded)
-
-        # Convert assemblyDecoded into actual objects
-
-        eq = assemblyEncoded == json.dumps(assemblyDecoded, sort_keys=False, default=self.encoder, indent=3)
-
-        if eq:
-            print("Strings are the same! Assembly Decode PASS")
-        else:
-            print("String are NOT the same! Assembly Decode FAIL")
-
     def encoder(self, obj):
         # what state should look like in json
         if isinstance(obj, State):
@@ -179,6 +153,7 @@ class Historian:
             dict = {}
             dict["history"] = obj.movelist
             dict["assembly"] = obj.assembly
+            dict["timetaken"] = obj.timetaken
 
             return dict
         elif isinstance(obj, Tile):
