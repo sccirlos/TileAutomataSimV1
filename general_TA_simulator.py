31743d99f2d6cbb87af3d9430005cef3c05f9fbf
Andrew Rodriguez <aroday23@gmail.com>
2021-11-02 13:58:52 -0500

    Revert merge

--- general_TA_simulator.py
+++ general_TA_simulator.py
@@ -124,10 +124,7 @@ class Ui_MainWindow(QMainWindow, TAMainWindow.Ui_MainWindow):
         self.Last_button.setIcon(QtGui.QIcon(
             'Icons/tabler-icon-player-skip-forward.png'))
 
-        self.New_button.clicked.connect(self.Click_newFile)
-
         self.Edit_button.clicked.connect(self.Click_EditFile)
-
         # "Quick Rotate"
         self.Rotate_button.clicked.connect(self.Click_QuickRotate)
 
@@ -974,12 +971,10 @@ class Ui_MainWindow(QMainWindow, TAMainWindow.Ui_MainWindow):
             self.e.show()
         else: 
             print("Please load a file to edit.")
+# add self, engine - then fill the table 
+# engine has the system 
 
-    def Click_newFile(self):
-        pass
-    # new file should open editor with no system loaded
-       # if (self.SysLoaded == False):
-            # open blank editor
+
 
 class Ui_EditorWindow(QMainWindow, EditorWindow16.Ui_EditorWindow):
     def __init__(self, engine, mainGUI):
@@ -988,7 +983,6 @@ class Ui_EditorWindow(QMainWindow, EditorWindow16.Ui_EditorWindow):
         self.mainGUI = mainGUI
         self.Engine = engine
         self.system = engine.system
-        
          # set row count state table
         self.newStateIndex = len(self.system.states)
         self.tableWidget.setRowCount(len(self.system.states))
@@ -1144,7 +1138,7 @@ class Ui_EditorWindow(QMainWindow, EditorWindow16.Ui_EditorWindow):
             r += 1
 
     
-         # action for 'apply' the changes made to the side edit window to the view states side 
+     # action for 'apply' the changes made to the side edit window to the view states side 
         self.pushButton.clicked.connect(self.Click_EditApply)
          # action for 'save' the changes made to the side edit window to the XML file
         self.pushButton_2.clicked.connect(self.Click_EditSaveAs)
@@ -1153,15 +1147,17 @@ class Ui_EditorWindow(QMainWindow, EditorWindow16.Ui_EditorWindow):
         self.pushButton_5.clicked.connect(self.Click_AddRowTrans)
          # user deletes state - currently only deletes state from 
          # state table. 
+         # need to delete from aff rules and trans rules too and 
+         # add the pop up message "are you sure delete state and its rules?"
         self.pushButton_11.clicked.connect(self.click_removeRowState)
         self.pushButton_10.clicked.connect(self.click_removeRowAff)
         self.pushButton_9.clicked.connect(self.click_removeRowTran)
        
 
-         # duplicate row
+        # duplicate row
         self.pushButton_6.clicked.connect(self.click_duplicateRowState)
 
-         # for 'add state'
+    # for 'add state'
     def cellchanged(self, row, col):
         # only do anything is we are in the color column (0)
         if col == 0:
@@ -1252,10 +1248,18 @@ class Ui_EditorWindow(QMainWindow, EditorWindow16.Ui_EditorWindow):
 
     # remove/delete rows from state table
     def click_removeRowState(self):
+
+        # debug print the selected rows and columns
+        # for i in self.tableWidget.selectedIndexes():
+        #     print("row", i.row(), "col", i.column())
+
         # only delete if there is something in the table, and if there is something selected
         if self.tableWidget.rowCount() > 0 and len(self.tableWidget.selectedIndexes()) > 0:
             self.tableWidget.removeRow(self.tableWidget.selectedIndexes()[0].row())
 
+        # want to delete state and associated aff/trans rules
+        # search through affinity and transitions rules & delete 
+        # the rows that contain that deleted state label.
     def click_removeRowAff(self):
         if self.tableWidget_2.rowCount() > 0 and len(self.tableWidget_2.selectedIndexes()) > 0:
             self.tableWidget_2.removeRow(self.tableWidget_2.selectedIndexes()[0].row())
@@ -1264,11 +1268,6 @@ class Ui_EditorWindow(QMainWindow, EditorWindow16.Ui_EditorWindow):
         if self.tableWidget_3.rowCount() > 0 and len(self.tableWidget_3.selectedIndexes()) > 0:
             self.tableWidget_3.removeRow(self.tableWidget_3.selectedIndexes()[0].row())
 
-    # not copying widget 
-    # to do: create a new widget, feed it data from previous
-    def copy_widget(self, w):
-
-        r = self.tableWidget.currentRow()
 
     def copy_widget(self, w):
         if isinstance(w, QtWidgets.QWidget):
@@ -1303,13 +1302,33 @@ class Ui_EditorWindow(QMainWindow, EditorWindow16.Ui_EditorWindow):
                     cells["items"].append((i, it.clone()))
 
                 w = self.tableWidget.cellWidget(currentRow, i)
-                
+                print(w)
                 if w:
                     cells["widgets"].append((i, self.copy_widget(w)))
             self.copy(cells, currentRow+1)
 
-           
-        
+            # adding a new row below selected row - use insert row
+            # ok inserts row right below the selected one- thats good 
+            #currentRow = self.tableWidget.selectedIndexes()[0].row()
+            #newrow = self.tableWidget.insertRow(currentRow+1)
+
+         
+    
+
+
+
+
+            #for j in range(columnCount):
+               #  self.tableWidget.setItem(newrow, j, QTableWidgetItem(self.tableWidget.item(currentRow, j).text()))
+
+            #for j in range(columnCount):
+             #   if not self.tableWidget(currentRow +1, j) is None:
+             #       self.setItem(currentRow +1, j, QTableWidgetItem(self.item.(currentRow+1, j).text()))
+
+
+
+
+
 
     def Click_EditApply(self):
 
@@ -1328,6 +1347,8 @@ class Ui_EditorWindow(QMainWindow, EditorWindow16.Ui_EditorWindow):
             color = color_cell.text()
             label = label_cell.text()
             
+            #print(initialCheckbox)
+            # 'apply as' works now
             initial = initialCheckbox.layout().itemAt(0).widget().isChecked()
             seed = seedCheckbox.layout().itemAt(0).widget().isChecked()
             s = State(label, color)
@@ -1383,15 +1404,16 @@ class Ui_EditorWindow(QMainWindow, EditorWindow16.Ui_EditorWindow):
         self.mainGUI.Update_available_moves()
 
 
-    
+     
     def Click_EditSaveAs(self):
-      print("Save As button clicked")
+        print("Save As button clicked")
        
-      fileName = QFileDialog.getSaveFileName(
-      self, "QFileDialog.getSaveFileName()", "", "XML Files (*.xml)")
+        if(self.SysLoaded == True):
+            fileName = QFileDialog.getSaveFileName(
+                self, "QFileDialog.getSaveFileName()", "", "XML Files (*.xml)")
 
-      if(fileName[0] != ''):
-        SaveFile.main(currentSystem, fileName)
+            if(fileName[0] != ''):
+                SaveFile.main(currentSystem, fileName)
 
 
 class Move(QWidget):
@@ -1439,9 +1461,13 @@ class Move(QWidget):
         self.mw.do_move(self.move)
 
         
+
+
+    
+
 if __name__ == "__main__":
      # App Stuff
-        app = QtWidgets.QApplication(sys.argv)
+        app = QApplication(sys.argv)
         w = Ui_MainWindow()
         w.show()
         sys.exit(app.exec_())
